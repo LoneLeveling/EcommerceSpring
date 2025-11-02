b294db83f76ab639b11fa960d612177f3e5d4796
Abhishek Sharma <abhioptcs2000@gmail.com>
2025-11-03 00:12:01 +0530

    Added controllers and services

---
+++ src/main/java/com/example/demo/controllers/CategoryControllerClass.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories/")
+public class CategoryControllerClass {
+    @GetMapping
+public String getCategory()
+{
+    return "Electronics";
+}
+@GetMapping("/count")
+    public int getCount()
+{
+    return 5;
+}
+
+@PostMapping
+public String getPostCategory()
+{
+    return "post Electronics";
+}
+
+}
