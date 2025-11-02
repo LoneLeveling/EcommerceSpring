8b3f8a2ac29980ab15ac55aa89998e0f6e38e051
Abhishek Sharma <abhioptcs2000@gmail.com>
2025-11-02 23:28:30 +0530

    Added .env setup and a demo restController class 'WelcomeController'

--- src/main/java/com/example/demo/EcommerceSpringApplication.java
+++ src/main/java/com/example/demo/EcommerceSpringApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
-
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class EcommerceSpringApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();//loads the .env file from root path
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);//set system proprties from env variables and set them.
 		SpringApplication.run(EcommerceSpringApplication.class, args);
 	}
-
 }
