04fb533b2adef53552a04778cbf17275ff204030
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-01-04 18:29:08 +0530

    code changes added to Application file

--- src/main/java/com/example/demo/EcommerceSpringApplication.java
+++ src/main/java/com/example/demo/EcommerceSpringApplication.java
@@ -3,13 +3,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+@SpringBootApplication //starter Annotation--> starting point/Entry point of your java app.
 public class EcommerceSpringApplication {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().load();//loads the .env file from root path
 		dotenv.entries().forEach(entry ->
 				System.setProperty(entry.getKey(), entry.getValue())
-		);//set system proprties from env variables and set them.
+		);//set system properties from env variables and set them.
 		SpringApplication.run(EcommerceSpringApplication.class, args);
 	}
 }
