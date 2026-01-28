b23b29af67eaaf28a2f1c902f4db0e305ddd8d67
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-01-28 14:29:20 +0530

    added code for controller

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,29 +1,30 @@
 package com.example.demo.controllers;
 
 import com.example.demo.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
     @GetMapping("/employees/{employeeId}")
-    public EmployeeDTO getEmployeebYiD(@PathVariable Long employeeId)
-    {
-        return new EmployeeDTO(employeeId,"Abhishek","abhi@gmail.com",25, LocalDate.of(2026,2,22),true);
+    public EmployeeDTO getEmployeebYiD(@PathVariable(name = "employeeId") Long Id) {
+        return new EmployeeDTO(Id, "Abhishek", "abhi@gmail.com", 25, LocalDate.of(2026, 2, 22), true);
     }
 
-    @GetMapping("/employees")
-    public String getEmployeeAge(@RequestParam (required = false) Integer age)
+    @GetMapping
+    public String getEmployeeAge(@RequestParam(required = false, name = "inputAge") Integer age,
+                                 @RequestParam(required = false) String sortBy)  //required = false <-- makes the field optional
     {
-        if(age==null) {
-            return "Enter a value for age";
+        {
+            if (age == null) {
+                return "Enter a value for age";
+            } else return "Hi the age is: " +age+ " "+sortBy;
+
         }
-        else return "Hi the age is: "+age;
     }
 }
-//“@RequestParam is required by default. Without required=false, Spring rejects missing parameters before the controller method is invoked, so null checks inside the method are ineffective.”
-//vvimp note: PathVariable conversion happens before controller execution.   If conversion fails, your method is never called.
+//“@RequestParam is required by default. Without required=false, Spring rejects missing parameters before the controller method is invoked,
+// so null checks inside the method are ineffective.”
+//vvimp note: PathVariable conversion happens before controller execution. If conversion fails, your method is never called.
